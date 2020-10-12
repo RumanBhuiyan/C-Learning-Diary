d3b12d0f7009b64e14be2a84b3b7a3c3e264426b
CSE <revengerreigns076@gmail.com>
2020-10-12 11:56:41 +0600

    Implementing Binary Search in my own way

---
+++ Data Structure/Searching/IterativeBinarySearch.cpp
@@ -0,0 +1,42 @@
+//Binary Search i)first sort the array ii)then search through it
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int iterativeBinarySearch(int numbers[],int size,int search);
+
+int main(){
+
+ int Numbers[]={30,23,5,9,2,1};
+ sort(Numbers,Numbers+sizeof(Numbers)/sizeof(int));// 1 2 5 9 23 30
+
+  int searchingNumber;
+  cout<<"Enter the number you want to search : ";
+  cin>>searchingNumber;
+
+  int size=sizeof(Numbers)/sizeof(int);
+  int index=iterativeBinarySearch(Numbers,size,searchingNumber);
+
+  cout<<index<<endl;
+
+  return 0;
+}
+
+int iterativeBinarySearch(int numbers[],int size,int search){
+
+ for(int i=0,begin=0,end=size-1,mid=size/2;i<size;i++){
+
+     if(numbers[mid]==search){
+       return mid;
+     }
+     else if(numbers[mid]<search){
+         begin=mid+1;
+         mid=(begin+end)/2;
+     }
+     else{
+         end=mid-1;
+         mid=(begin+end)/2;
+     }
+  }
+  return -1;
+}
---
+++ Data Structure/Searching/binarySearch.cpp
@@ -0,0 +1,66 @@
+//Binary Search i)first sort the array ii)then search through it
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int iterativeBinarySearch(int numbers[],int size,int search);
+int recursiveBinarySearch(int numbers[],int begin,int end,int mid,int search);
+
+int main(){
+
+ int Numbers[]={30,23,5,9,2,1};
+ sort(Numbers,Numbers+sizeof(Numbers)/sizeof(int));// 1 2 5 9 23 30
+
+  int searchingNumber;
+  cout<<"Enter the number you want to search : ";
+  cin>>searchingNumber;
+
+  int size=sizeof(Numbers)/sizeof(int);
+  int index;
+  index=iterativeBinarySearch(Numbers,size,searchingNumber);
+
+  cout<<"Iterative Binary Search : "<<index<<endl;
+
+  index=recursiveBinarySearch(Numbers,0,size-1,size/2,searchingNumber);
+
+  cout<<"Recursive Binary Search : "<<index<<endl;
+
+  return 0;
+}
+
+int iterativeBinarySearch(int numbers[],int size,int search){
+
+ for(int i=0,begin=0,end=size-1,mid=size/2;i<size;i++){
+
+     if(numbers[mid]==search){
+       return mid;
+     }
+     else if(numbers[mid]<search){
+         begin=mid+1;
+         mid=(begin+end)/2;
+     }
+     else{
+         end=mid-1;
+         mid=(begin+end)/2;
+     }
+  }
+  return -1;
+}
+
+int recursiveBinarySearch(int numbers[],int begin,int end,int mid,int search){
+
+   if(end<begin){
+      return -1;
+   }
+    else if(numbers[mid]==search){
+        return mid;
+    }
+    else if(numbers[mid]<search){
+     return recursiveBinarySearch(numbers,mid+1,end,(mid+1+end)/2,search);
+    }
+    else {
+     return recursiveBinarySearch(numbers,begin,mid-1,(begin+mid-1)/2,search);
+    }
+
+ return -1;
+}
